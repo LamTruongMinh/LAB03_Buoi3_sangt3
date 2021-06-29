85a4741042d49c42e9df0dae94cb813e1d18822a
2009tude@gmail.com <2009tude@gmail.com>
2021-06-29 12:02:02 +0700

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LAB03_1.MvcApplication" Language="C#" %>
---
+++ ViewModels/CourseViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using LAB03_1.Models;
+
+namespace LAB03_1.ViewModels
+{
+    public class CourseViewModel
+    {
+        [Required]
+        public string Place { get; set; }
+        [Required]
+        [FutureDate]
+        public string Date { get; set; }
+        [Required]
+        [FutureTime]
+        public string Time { get; set; }
+        [Required]
+
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0}   {1}   {2}  ", Date, Time));
+        }
+
+    }
+}
